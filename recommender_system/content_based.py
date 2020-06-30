47649db32e6ec50e021f0987d58565ca54e3c12f
Marvin <marvinchui@gmail.com>
2020-06-30 13:33:08 +1000

    added content based RS, updated CF RS

--- recommender_system/content_based.py
+++ recommender_system/content_based.py
@@ -4,29 +4,41 @@ import os
 import pandas as pd
 import numpy as np
 
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 dataset_path = "dataset/goodbooks-10k"
 
-class CFRecommenderSystem:
+class ContextRecommenderSystem:
     def __init__(self):
         self.load_dataset(dataset_path)
         self.similarity_matrix = self.fit()
+        
 
     def load_dataset(self, dataset_path):
-    
+
         # Paths
-        ratings_path = os.path.join(dataset_path, "ratings.csv")
         books_path = os.path.join(dataset_path, "books.csv")
+        tags_path = os.path.join(dataset_path, "tags.csv")
+        book_tags_path = os.path.join(dataset_path, "book_tags.csv")
+        
+        # Load tags dataset
+        self.tags = pd.read_csv(tags_path)
+        
+        # Load book tags dataset
+        self.book_tags = pd.read_csv(book_tags_path)
 
         # Load books dataset
         books = pd.read_csv(books_path, encoding = "ISO-8859-1")
         books['author'] = books['authors'].apply(lambda x:  x.split(',')[0]) # Only use name of main author
         books = books[['book_id', 'goodreads_book_id', 'title', 'average_rating', 'author']]
-        self.books = books
         
-        # Load ratings dataset
-        self.ratings = pd.read_csv(ratings_path)
+        all_features = pd.merge(self.book_tags, self.tags, on='tag_id')
+        all_features = pd.merge(books, all_features, on='goodreads_book_id')
+        all_features = all_features.groupby('book_id')['tag_name'].apply(' '.join).reset_index()
+        books['all_features'] = books['author'].apply(lambda x: x.replace(' ','')) + ' ' + books['title'] + ' ' + all_features['tag_name']
+
+        self.books = books
         
         # Mapping from index to book_id and vice versa
         self.index_to_book = {}
@@ -35,19 +47,15 @@ class CFRecommenderSystem:
           self.index_to_book[index] = book_id
           self.book_to_index[book_id] = index
         
-        
     def fit(self):
+        vectorizer = TfidfVectorizer(analyzer='word',ngram_range=(1,1),min_df=0.002, stop_words='english')
+        book_feature_matrix = vectorizer.fit_transform(self.books['all_features'])
+        similarity_matrix = linear_kernel(book_feature_matrix, book_feature_matrix)
 
-        user_book_matrix = self.ratings.pivot_table(index='book_id', columns='user_id', values='rating')
-        user_book_matrix.fillna(0, inplace=True)
-        similarity_matrix = cosine_similarity(user_book_matrix.to_numpy())
-        
         return similarity_matrix
-        
-    
     
     def get_recommendations(self, book_id, count=5, verbose=True):
-        print("Input Book:")
+        print("Input book:")
         print(self.books[self.books['book_id'] == book_id][['book_id', 'title', 'author', 'average_rating']].set_index('book_id').to_string())
         idx = self.book_to_index[book_id]
         book_indices = np.argsort(self.similarity_matrix[idx])[::-1][1:count + 1]
@@ -62,5 +70,5 @@ class CFRecommenderSystem:
         
 
 if __name__ == '__main__':
-    rs = CFRecommenderSystem()
+    rs = ContextRecommenderSystem()
     recommendations = rs.get_recommendations(99, 5)
