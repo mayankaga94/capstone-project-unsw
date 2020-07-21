dbd6827981e60c4f4448b7a3bebf5827ecfda94c
neelabhs91 <neelabhs91@gmail.com>
2020-07-21 22:39:40 +1000

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+//-------------------------------middlewares---------------------------------//
+
+app.use(bodyParser.json())
+
+
+
+//-------------------------------routes--------------------------------------//
+
+app.use('/user',require('./routes/user'))
+
+//------------------------ starting the server ------------------------------//
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT)
+console.log(`Server started on port ${PORT}`);
+
+// --------------------------------END--------------------------------------//
