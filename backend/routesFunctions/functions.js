d9193c1949518b08bd8d66a3773b071e83246094
Mayank Agarwal <mayankaga94@gmail.com>
2020-07-27 16:04:08 +1000

    commit

--- backend/routesFunctions/functions.js
+++ backend/routesFunctions/functions.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require('../config/sql_config')
-const Review = require('../models/Review')
 
 module.exports = {
     // next is not needed at the moment 
